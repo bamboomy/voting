3496adbdc9c730dcc1172662e7290e26edf97c88
Sander Eraser <bamboomy@gmail.com>
2018-06-17 20:56:52 +0200

    green

---
+++ technologies.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+	<center>
+		<h1>What are your interests?</h1>
+		<table>
+			<tr>
+				<td>Technology</td><td>vote</td><td>votes</td>
+			</tr>
+			<tr>
+				<td>Java</td>
+				<td style="color:green;">+1</td>
+				<td>0</td>
+			</tr>
+			<tr>
+				<td>Python</td>
+				<td style="color:green;">+1</td>
+				<td>0</td>
+			</tr>
+		</table>
+	</center>
+</body>
+</html>
