d8eaec8593f00c510f734efed678dd9c3de13252
Sander Eraser <bamboomy@gmail.com>
2018-06-17 19:31:20 +0200

    error

--- login.php
+++ login.php
@@ -14,6 +14,26 @@ if ($conn->connect_error) {
 	die;
 } 
 
+if(isset($_POST['email'])){
+
+	$sql = "select id from users where email = '".test_input($_POST['email'])."' and passwoord = '".test_input($_POST['password'])."';";
+
+	$result = $conn->query($sql);
+
+	if($result->num_rows == 1){
+		
+		$row = $result->fetch_assoc();
+		
+		$_SESSION['id'] = $row['id'];
+
+		header("Location: technologies.html");
+		
+		die;
+	}
+	
+	$error = "not found";
+}
+
 ?>
 <html>
 <body>
@@ -21,11 +41,20 @@ if ($conn->connect_error) {
 		<h1>Please log in...</h1>
 		<form class="form-inline" id="subscribeAppForm" action="login.php" method="post">
 			<table>
+<?
+if(isset($error)){
+?>			
 				<tr>
-					<td>Email:</td><td><input type="text" /></td>
+					<td colspan="2">We couldn't find this email/password combination. </td>
+				</tr>
+<?
+}
+?>
+				<tr>
+					<td>Email:</td><td><input type="text" name="email" /></td>
 				</tr>
 				<tr>
-					<td>Password:</td><td><input type="password" /></td>
+					<td>Password:</td><td><input type="password" name="password" /></td>
 				</tr>
 				<tr>
 					<td/><td style="text-align:right;"><button type="submit">Login</button></td>
